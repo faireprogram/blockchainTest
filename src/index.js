43b653a8e893e9a6265c4d2e05076ec10d643906
Jack Zhang <jack.zhang@chase.com>
2019-05-16 09:24:48 -0400

    update the router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import FormContainer from './js/components/container/FormContainer.jsx';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router';
 import { ConnectedRouter } from 'connected-react-router';
 import configureStore, { history } from './js/configureStore';
 
@@ -12,9 +12,11 @@ ReactDom.render(
 	<Provider store={store}>
 		<ConnectedRouter history={history}>
 			<Switch>
+				<Route exact path="/" component={FormContainer} />
 				<Route exact path="/search1" component={FormContainer} />
 				<Route exact path="/search2" render={()=>{return (<div>search2</div>);}} />
-				<Route render={()=>{return (<div>Not Found</div>);}} />
+				<Route path="/notFound" render={()=>{return (<div>Not Found</div>);}} />
+				<Redirect to="/notFound"/>
 			</Switch>
 		</ConnectedRouter>
 	</Provider>,
